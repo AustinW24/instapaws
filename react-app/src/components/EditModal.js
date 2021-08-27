b0063583e253d78b17498b99a1ae7a35e6a7ea85
Austin Williams <austinwill264@gmail.com>
2021-08-27 13:47:50 -0700

    trailing slash for create

--- react-app/src/components/EditModal.js
+++ react-app/src/components/EditModal.js
@@ -5,5 +5,17 @@ import Modal from '../context/Modal'
 
 
 function EditModal({ post, setShowModal }) {
+    const dispatch = useDispatch();
+    const [caption, setCaption] = useState('');
 
+
+    return(
+        <>
+        <form className="editmodal-form">
+            <div>
+                
+            </div>
+        </form>
+        </>
+    )
 }
