5e2f3565bf27a9a2ef019e166ef968f3795d908a
Austin Williams <austinwill264@gmail.com>
2021-08-30 14:13:20 -0700

    removed comments ap

--- app/api/comment_routes.py
+++ app/api/comment_routes.py
@@ -1,40 +1,40 @@
-from flask import Blueprint, request
-from ..models.post import Post
-from ..forms.post_form import PostForm
-from ..models.db import db
-# from app.forms import PostForm, CommentForm
-from datetime import datetime
-from flask_login import login_required, current_user
+# from flask import Blueprint, request
+# from ..models.post import Post
+# from ..forms.post_form import PostForm
+# from ..models.db import db
+# # from app.forms import PostForm, CommentForm
+# from datetime import datetime
+# from flask_login import login_required, current_user
 
-post_routes = Blueprint('comments', __name__)
-
-
-@post_routes.route('/')
-def get_all_posts():
-    posts = Post.query.all()
-
-    return {"posts": {post.id: post.to_dict() for post in posts}}
+# post_routes = Blueprint('comments', __name__)
 
 
+# @post_routes.route('/')
+# def get_all_posts():
+#     posts = Post.query.all()
 
-@post_routes.route('/', methods=['GET', 'POST'])
-@login_required
-def createPost():
-    form = PostForm()
-    user = current_user
-    form['csrf_token'].data = request.cookies['csrf_token']
+#     return {"posts": {post.id: post.to_dict() for post in posts}}
 
-    print('before validate', form.data)
-    if form.validate_on_submit():
-        new_post = Post(
-            user_id=user.id,
-            caption=form.data['caption'],
-            picture_url=form.data['picture_url'],
-            timestamp=datetime.now()
-        )
 
-        db.session.add(new_post)
-        db.session.commit()
-        print('inside validation p', new_post.to_dict())
-        return {'posts': [new_post.to_dict()]}
-    return {'errors': [form.errors]}
+
+# @post_routes.route('/', methods=['GET', 'POST'])
+# @login_required
+# def createPost():
+#     form = PostForm()
+#     user = current_user
+#     form['csrf_token'].data = request.cookies['csrf_token']
+
+#     print('before validate', form.data)
+#     if form.validate_on_submit():
+#         new_post = Post(
+#             user_id=user.id,
+#             caption=form.data['caption'],
+#             picture_url=form.data['picture_url'],
+#             timestamp=datetime.now()
+#         )
+
+#         db.session.add(new_post)
+#         db.session.commit()
+#         print('inside validation p', new_post.to_dict())
+#         return {'posts': [new_post.to_dict()]}
+#     return {'errors': [form.errors]}
